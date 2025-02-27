04b695d2f17b5396a7e53d84c410bfa69bff9a98
vanessatelter08 <vanessa.stelter.silva@escola.pr.gov.br>
2025-02-27 12:01:21 -0300

    Add files via upload

---
+++ vanesao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int numero = 5;  
+int chute;
+printf("digite um numero:");
+scanf("%d", &chute);
+if(chute == numero) {
+    printf("acertou! \n");
+}else{
+}
+printf("errou!! bobão! \n");
+printf("o numero foi %d" , numero);
+    return 0;
+}
